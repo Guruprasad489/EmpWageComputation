6330e35466a4bddfdcc62034de52619922f71a22
Guruprasad489 <guruprasadk.dev@gmail.com>
2022-01-28 23:19:16 +0530

    [Guru] Added Emp Wage for Multiple Companies -UC8

--- EmpWageComputation/EmpAttendance.cs
+++ EmpWageComputation/EmpAttendance.cs
@@ -8,12 +8,13 @@ namespace EmpWageComputation
 {
     public class EmpAttendance
     {
-        const int EMP_FULLTIME=1, EMP_PARTTIME=2, EMP_WAGE_PER_HR=20, WORKING_DAYS_PER_MONTH=20, MAX_HRS_PER_MONTH=100;
-        static int daily_Emp_Wage=0, empHrs=0, total_Emp_Wage=0, total_Emp_Hrs=0, day=1;
-        public static void GetMonthlyEmpWage()
+        public const int EMP_FULLTIME=1, EMP_PARTTIME=2;
+        public static void GetMonthlyEmpWage(string company, int emp_Wage_Per_Hr, int working_Days_Per_Month, int max_Hrs_Per_Month)
         {
+            int empHrs = 0, total_Emp_Hrs = 0, totalWorkingDays = 1, total_Emp_Wage = 0, daily_Emp_Wage = 0;
+
             Random random = new Random();
-            while (day <= WORKING_DAYS_PER_MONTH && total_Emp_Hrs <= MAX_HRS_PER_MONTH)
+            while (totalWorkingDays <= working_Days_Per_Month && total_Emp_Hrs <= max_Hrs_Per_Month)
             {
                 int randomInput = random.Next(0, 3);
                 switch (randomInput)
@@ -28,14 +29,14 @@ namespace EmpWageComputation
                         empHrs = 0;
                         break;
                 }
-                daily_Emp_Wage = empHrs * EMP_WAGE_PER_HR;
-                Console.WriteLine("Employee wage for day {0} is: {1} for {2} Hrs", day, daily_Emp_Wage, empHrs);
+                daily_Emp_Wage = empHrs * emp_Wage_Per_Hr;
+                Console.WriteLine("Employee wage for day {0} is: {1} for {2} Hrs", totalWorkingDays, daily_Emp_Wage, empHrs);
                 total_Emp_Wage += daily_Emp_Wage;
                 total_Emp_Hrs += empHrs;
-                day++;
+                totalWorkingDays++;
             }
-            Console.WriteLine("Total Days: {0}, Total working hours: {1}", (day-1), total_Emp_Hrs);
-            Console.WriteLine("Total Employee Wage is: " +total_Emp_Wage);
+            Console.WriteLine("Total Days: {0}, Total working hours: {1}", (totalWorkingDays-1), total_Emp_Hrs);
+            Console.WriteLine("Total Employee Wage is: " +total_Emp_Wage + "\n");
         }
 
     }
--- EmpWageComputation/Program.cs
+++ EmpWageComputation/Program.cs
@@ -10,9 +10,11 @@ namespace EmpWageComputation
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Welcome to Empolyee Wage Computation Program");
+            Console.WriteLine("Welcome to Empolyee Wage Computation Program\n");
 
-            EmpAttendance.GetMonthlyEmpWage();
+            EmpAttendance.GetMonthlyEmpWage("Google", 20, 2, 10);
+            EmpAttendance.GetMonthlyEmpWage("Microsoft", 10, 4, 20);
+            EmpAttendance.GetMonthlyEmpWage("Deloitte", 15, 3, 15);
             Console.ReadLine();
         }
     }
