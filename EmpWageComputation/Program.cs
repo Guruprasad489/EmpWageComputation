75d6ccc24a1ee7ebef6d0283e6ea26b7ad23fcdb
Guruprasad489 <guruprasadk.dev@gmail.com>
2022-01-31 20:03:01 +0530

    [Guru] Modifeied to save total wage by company -UC9

--- EmpWageComputation/Program.cs
+++ EmpWageComputation/Program.cs
@@ -12,9 +12,18 @@ namespace EmpWageComputation
         {
             Console.WriteLine("Welcome to Empolyee Wage Computation Program\n");
 
-            EmpAttendance.GetMonthlyEmpWage("Google", 20, 2, 10);
-            EmpAttendance.GetMonthlyEmpWage("Microsoft", 10, 4, 20);
-            EmpAttendance.GetMonthlyEmpWage("Deloitte", 15, 3, 15);
+            EmpAttendance google = new EmpAttendance("Google", 10, 4, 20);
+            google.GetMonthlyEmpWage();
+            Console.WriteLine(google.ToString());
+
+            EmpAttendance microsoft = new EmpAttendance("Microsoft", 20, 5, 20);
+            microsoft.GetMonthlyEmpWage();
+            Console.WriteLine(microsoft.ToString());
+
+            EmpAttendance deloitte = new EmpAttendance("Deloitte", 30, 3, 30);
+            deloitte.GetMonthlyEmpWage();
+            Console.WriteLine(deloitte.ToString());
+
             Console.ReadLine();
         }
     }
